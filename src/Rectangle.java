f5e651b9f607bb355dc9d79766f3078e6a4bddc0
sliu-01 <42978561+sliu-01@users.noreply.github.com>
2018-10-03 00:31:30 -0400

    Add files via upload

---
+++ src/Rectangle.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Rectangle 
+{
+
+	public static void main(String[] args) 
+	{
+		Scanner in = new Scanner(System.in);
+		
+		System.out.print("Please enter the width value: ");
+		double width = in.nextDouble();
+		in.nextLine();
+		
+		System.out.print("Please enter the length value: ");
+		double length = in.nextDouble();
+		in.nextLine();
+		
+		double area = width * length;
+		System.out.printf("%-50s %10.2f\n", "The area of the given rectangle is: ", area);
+		
+		double perimeter = 2 * (width + length);
+		System.out.printf("%-50s %10.2f\n","The perimeter of the given rectangle is: ", perimeter);
+		
+		double diagonal = Math.sqrt(Math.pow(width, 2) + Math.pow(length, 2));
+		System.out.printf("%-50s %10.2f\n", "The diagonal of the given rectangle is: ", diagonal);
+
+	}
+
+}
